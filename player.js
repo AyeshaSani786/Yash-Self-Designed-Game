a792238bec01b73582074f91b174bae9f68cb035
AyeshaSani786 <72201814+AyeshaSani786@users.noreply.github.com>
2021-06-01 16:18:08 +0530

    Add files via upload

---
+++ player.js
@@ -0,0 +1,38 @@
+class Player{
+    constructor(){
+        this.index = null;
+        this.PlayerId = null;
+        this.distance = 0;
+         this.name = null;
+    }
+
+    getCount(){
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value",function(data){
+        playerCount = data.val();
+        })
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+       playerCount: count
+        })
+    }
+
+ update(){
+     var playerIndex ="players/player " +  this.index; 
+    
+     database.ref(playerIndex).set({
+         PlayerId : this.PlayerId,
+         distance:this.distance
+     });
+ }
+
+ static getPlayerInfo(){
+     var playerInfoRef = database.ref('players');
+     playerInfoRef.on("value",(data)=>{
+     allPlayers = data.val();
+     })
+ }
+
+}
---
+++ sketch.js
@@ -0,0 +1,72 @@
+var canvas;
+var shooter, shooter1,shooter2;
+//var garden;
+//var Obstacle;
+var playerCount;
+var gameState = 0;
+var database;
+var allPlayers;
+var form,game,player;
+
+
+function preload() {
+track = loadImage("track.png")
+}
+
+function setup(){
+    var canvas = createCanvas(1200,1200);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+   // garden =  createSprite(900,700,50,50);
+   //garden.addImage(gardenImg) 
+  // garden.velocityY = 4 ; 
+    
+   //shooter = createSprite(800,700,10, 15);
+  // shooter.shapeColor = "red";
+   
+   
+}
+
+
+function draw(){
+    background("white");
+    //image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+    
+    if(playerCount === 2){
+       game.update(1);
+    }
+    if(gameState ===1){
+         clear();
+         game.play();
+    }
+}
+  //  
+   // if(keyDown(LEFT_ARROW)){
+   //     shooter.x=shooter.x -3
+   // }
+   // if(keyDown(UP_ARROW)){
+  //      shooter.y=shooter.y -3
+  //  }
+    
+  //  if(keyDown(RIGHT_ARROW)){
+       // shooter.x=shooter.x +3
+  //  }
+  //  if(keyDown(DOWN_ARROW)){
+   //     shooter.y=shooter.y +3
+   // }
+
+   // spawnObstacle();
+   // drawSprites();
+    
+//}
+//spawnObstacle();{
+
+   // if(frameCount % 60 ===0){
+       //  Obstacle = createSprite(400,200,10,20)
+  //  }
+
+//}
+
+
